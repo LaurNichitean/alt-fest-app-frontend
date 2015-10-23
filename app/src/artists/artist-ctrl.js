321e11126f2ef9f218294d96b9e37444a3c61f6d
laurnichitean <bv802324>
2015-10-23 23:14:07 +0300

    ALT Fest App - Step 3 - Added functionality to add or remove songs

--- app/src/artists/artist-ctrl.js
+++ app/src/artists/artist-ctrl.js
@@ -5,7 +5,7 @@
   'use strict';
   angular.module('altFest').controller('ArtistCtrl', ArtistCtrl);
 
-  function ArtistCtrl($scope, $mdSidenav, ArtistService, SongService) {
+  function ArtistCtrl($scope, $mdToast, $mdSidenav, ArtistService, SongService) {
     ArtistService.query(function (result) {
       $scope.artists = result;
       $scope.selectedArtist = result[0];
@@ -18,10 +18,6 @@
         angular.extend($scope.selectedArtist, {
           songs: result
         });
-        $scope.selectedArtist.songs = $scope.selectedArtist.songs.filter(function (song) {
-          // modify url's to embed
-          return song.urlYoutube = song.urlYoutube.replace('watch?v=', 'embed/');
-        });
       });
     }
 
@@ -29,6 +25,115 @@
       song.likes += value;
       SongService.song().update({id: song._id}, {"likes": song.likes});
     };
+
+    // we need the 'selectedArtist' variable to be defined on the scope
+    $scope.$watch('selectedArtist', function (nv, ov) {
+      if (nv) {
+        $scope.songToAdd = {
+          name: '',
+          likes: 0,
+          urlYoutube: '',
+          artistId: $scope.selectedArtist._id
+        };
+      }
+    });
+
+    // track changes on song to be added to list
+    $scope.$watch('songToAdd', function (nv, ov) {
+      console.log('songToAdd', nv);
+    }, true);
+
+    /* Add song */
+    $scope.addSongInputVisible = false;
+
+    // toggle visibility of the 'Add song' section
+    $scope.toggleAddSong = function () {
+      $scope.addSongInputVisible = !$scope.addSongInputVisible;
+    };
+
+    // persist creation of a new song
+    $scope.saveSong = function () {
+      // modify url to the 'embed' format
+      $scope.songToAdd.urlYoutube = $scope.songToAdd.urlYoutube.replace('watch?v=', 'embed/');
+      // send 'POST' request using 'save' method on our service
+      SongService.song().save($scope.songToAdd).$promise.then(function (result) {
+        var saveSongMessage;
+        // if the result has no message, it is successful
+        if (result.msg === '' && result.insertedIds[0].length) {
+          // get new object from backend
+          SongService.song().get({id: result.insertedIds[0]}).$promise.then(function (result) {
+            // add newly created song to the beginning of the list
+            $scope.selectedArtist.songs.unshift(result);
+            // reset model values
+            $scope.songToAdd.name = '';
+            $scope.songToAdd.urlYoutube = '';
+          });
+          saveSongMessage = 'Song was successfully added to the list.';
+        } else {
+          saveSongMessage = 'There was an error adding the song.';
+        }
+        // show notification with save result
+        $mdToast.show(
+          $mdToast.simple()
+            .content(saveSongMessage)
+            .position($scope.getToastPosition())
+            .hideDelay(3000)
+        );
+        $scope.addSongInputVisible = false;
+      });
+    };
+
+    /* Delete song */
+
+    // persist deletion of a song
+    $scope.deleteSong = function (song, index) {
+      // send 'DELETE' request using our service
+      SongService.song().delete({id: song._id}).$promise.then(function (result) {
+        var deleteSongMessage;
+        if (result.msg === '') {
+          deleteSongMessage = 'Song successfully deleted!';
+          // remove song element from array
+          $scope.selectedArtist.songs.splice(index, 1);
+        } else {
+          deleteSongMessage = 'The was an error deleting the song!';
+        }
+        // show notification with delete result
+        $mdToast.show(
+          $mdToast.simple()
+            .content(deleteSongMessage)
+            .position($scope.getToastPosition())
+            .hideDelay(3000)
+        );
+      });
+    };
+
+    /* Toast - notifications section */
+
+    var last = {
+      bottom: false,
+      top: true,
+      left: false,
+      right: true
+    };
+    $scope.toastPosition = angular.extend({}, last);
+    $scope.getToastPosition = function () {
+      sanitizePosition();
+      return Object.keys($scope.toastPosition)
+        .filter(function (pos) {
+          return $scope.toastPosition[pos];
+        })
+        .join(' ');
+    };
+    function sanitizePosition() {
+      var current = $scope.toastPosition;
+      if (current.bottom && last.top) current.top = false;
+      if (current.top && last.bottom) current.bottom = false;
+      if (current.right && last.left) current.left = false;
+      if (current.left && last.right) current.right = false;
+      last = angular.extend({}, current);
+    }
+
+    /* Artist section */
 
     $scope.$watch('selectedArtist', function (newValue, oldValue) {
       if (angular.isDefined(oldValue) && !angular.equals(newValue, oldValue)) {
@@ -45,5 +150,5 @@
     };
   }
 
-  ArtistCtrl.$inject = ['$scope', '$mdSidenav', 'ArtistService', 'SongService'];
+  ArtistCtrl.$inject = ['$scope', '$mdToast', '$mdSidenav', 'ArtistService', 'SongService'];
 })();
