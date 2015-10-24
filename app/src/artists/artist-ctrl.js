03b496644a3252ce83135048921008a5aa45be0d
laurnichitean <bv802324>
2015-10-24 12:39:41 +0300

    ALT Fest App - Step 4 - Added functionality to add and remove artists

--- app/src/artists/artist-ctrl.js
+++ app/src/artists/artist-ctrl.js
@@ -6,20 +6,64 @@
   angular.module('altFest').controller('ArtistCtrl', ArtistCtrl);
 
   function ArtistCtrl($scope, $mdToast, $mdSidenav, ArtistService, SongService) {
+    $scope.artists = [];
     ArtistService.query(function (result) {
-      $scope.artists = result;
-      $scope.selectedArtist = result[0];
-      // get list of songs for current selected artist
-      getArtistSongList();
+      if (result.length) {
+        $scope.artists = result;
+        $scope.selectedArtist = result[0];
+        // get list of songs for current selected artist
+        getArtistSongList();
+      }
     });
 
     function getArtistSongList() {
-      SongService.getListFromArtist().query({id: $scope.selectedArtist._id}).$promise.then(function (result) {
-        angular.extend($scope.selectedArtist, {
-          songs: result
+      if ($scope.selectedArtist) {
+        SongService.getListFromArtist().query({id: $scope.selectedArtist._id}).$promise.then(function (result) {
+          angular.extend($scope.selectedArtist, {
+            songs: result
+          });
+          $scope.selectedArtist.songs = $scope.selectedArtist.songs.filter(function (song) {
+            // modify url's to embed
+            return song.urlYoutube = song.urlYoutube.replace('watch?v=', 'embed/');
+          });
         });
-      });
+      }
     }
+
+    $scope.addArtistInputVisible = false;
+
+    $scope.toggleAddArtist = function () {
+      $scope.addArtistInputVisible = !$scope.addArtistInputVisible;
+    };
+
+    $scope.artistToAdd = {
+      name: ''
+    };
+
+    $scope.addArtist = function () {
+      ArtistService.save($scope.artistToAdd).$promise.then(function (result) {
+        if (result.status === 201) {
+          console.log('artist result', result);
+          // get newly created artist
+          ArtistService.get({id: result.insertedIds[0]}).$promise.then(function (result) {
+            // hide add artist input
+            $scope.toggleAddArtist();
+            // add artist to list
+            $scope.artists.push(result);
+            // reset artistToAdd object
+            $scope.artistToAdd.name = '';
+          })
+        }
+      })
+    };
+
+    $scope.deleteArtist = function (artist, index) {
+      ArtistService.delete({id: artist._id}).$promise.then(function (result) {
+        $scope.artists.splice(index, 1);
+        // deselect selected artist
+        $scope.selectArtist();
+      });
+    };
 
     $scope.addVote = function (song, value) {
       song.likes += value;
